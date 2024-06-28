a1fc086c82af7a70c5d89576c06bc21be961e8a4
Diogo Guedes <diogo.guedes111@gmail.com>
2024-06-28 11:03:24 +0100

    test

--- main.tf
+++ main.tf
@@ -1,21 +1,41 @@
+locals {
+  project_id = "arctic-ocean-427610-u4"
+}
+
 provider "google" {
-  project = "arctic-ocean-427610-u4"
+  project = local.project_id
   region  = "us-central1"
 }
 
-
-resource "google_compute_instance" "default" {
-  name         = "default"
-  machine_type = "e2-micro"
-  zone         = "us-central1-a"
-
-  network_interface {
-    network = "default"
+resource "google_iam_workload_identity_pool" "github_actions" {
+  project                   = local.project_id
+  workload_identity_pool_id = "github-actions"
+  display_name              = "GitHub Actions pool"
+  description               = "Workload Identity Pool managed by Terraform"
+  disabled                  = false
+}
+resource "google_iam_workload_identity_pool_provider" "github_actions" {
+  project                            = local.project_id
+  workload_identity_pool_id          = google_iam_workload_identity_pool.github_actions.workload_identity_pool_id
+  workload_identity_pool_provider_id = "github-actions"
+  display_name                       = "GitHub Actions provider"
+  description                        = "Workload Identity Pool Provider managed by Terraform"
+  attribute_condition                = "attribute.repository_owner==\"diogobytes\""
+  attribute_mapping = {
+    "google.subject"             = "assertion.sub"
+    "attribute.actor"            = "assertion.actor"
+    "attribute.aud"              = "assertion.aud"
+    "attribute.repository"       = "assertion.repository"
+    "attribute.repository_owner" = "assertion.repository_owner"
   }
-
-  boot_disk {
-    initialize_params {
-      image = "ubuntu-os-cloud/ubuntu-2004-focal-v20220712"
-    }
+  oidc {
+    allowed_audiences = []
+    issuer_uri        = "https://token.actions.githubusercontent.com"
   }
+}
+
+resource "google_service_account_iam_member" "wif-sa" {
+  service_account_id = "projects/arctic-ocean-427610-u4/serviceAccounts/gh-actions-wif@arctic-ocean-427610-u4.iam.gserviceaccount.com"
+  role               = "roles/iam.workloadIdentityUser"
+  member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github_actions.name}/*"
 }
