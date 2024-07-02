390282ef0d70262198df09972810ad68daa5034a
Diogo Guedes <diogo.guedes111@gmail.com>
2024-07-02 13:40:07 +0100

    teste

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ provider "google" {
 
 resource "google_iam_workload_identity_pool" "github_actions" {
   project                   = local.project_id
-  workload_identity_pool_id = "github-actions"
+  workload_identity_pool_id = "gh-test"
   display_name              = "GitHub Actions pool"
   description               = "Workload Identity Pool managed by Terraform"
   disabled                  = false
@@ -18,10 +18,9 @@ resource "google_iam_workload_identity_pool" "github_actions" {
 resource "google_iam_workload_identity_pool_provider" "github_actions" {
   project                            = local.project_id
   workload_identity_pool_id          = google_iam_workload_identity_pool.github_actions.workload_identity_pool_id
-  workload_identity_pool_provider_id = "github-actions"
+  workload_identity_pool_provider_id = "gh-test-provider"
   display_name                       = "GitHub Actions provider"
   description                        = "Workload Identity Pool Provider managed by Terraform"
-  attribute_condition                = "attribute.repository_owner==\"${local.gh_username}\""
   attribute_mapping = {
     "google.subject"             = "assertion.sub"
     "attribute.actor"            = "assertion.actor"
@@ -35,8 +34,20 @@ resource "google_iam_workload_identity_pool_provider" "github_actions" {
   }
 }
 
+resource "google_service_account" "service_account" {
+  account_id   = "gh-test"
+  display_name = "GH Service Account"
+}
+
 resource "google_service_account_iam_member" "wif-sa" {
-  service_account_id = "projects/arctic-ocean-427610-u4/serviceAccounts/gh-actions-wif@arctic-ocean-427610-u4.iam.gserviceaccount.com"
+  service_account_id = google_service_account.service_account.id
   role               = "roles/iam.workloadIdentityUser"
   member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github_actions.name}/*"
+}
+
+
+resource "google_project_iam_member" "this" {
+  project = local.project_id
+  role    = "roles/cloudsql.admin"
+  member  = google_service_account.service_account.member
 }
